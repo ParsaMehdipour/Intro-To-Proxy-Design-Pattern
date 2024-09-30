3f051f2d71dd7adb9fef2948110dd7904ccd0c6d
Parsa <mehdipourparsa@gmail.com>
2024-09-30 15:51:07 -0400

    feat : add interfaces

---
+++ DemoLibrary/Interfaces/IWeatherService.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoLibrary.Interfaces;
+
+/// <summary>
+/// The Subject interface declares common operations for both RealSubject and
+/// the Proxy. As long as the client works with RealSubject using this
+/// interface, you'll be able to pass it a proxy instead of a real subject.
+/// </summary>
+public interface IWeatherService
+{
+    Task<string> GetWeatherAsync(string city);
+    Task<int> GetTemperatureAsync(string city);
+    Task<List<string>> GetForecastAsync(string city);
+}
